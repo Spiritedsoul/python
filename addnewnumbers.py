d777e76aac2d1546aa3e68bec684faa0fddb9b67
spiritedsoul <nilabhadas0157@gmail.com>
2024-07-05 10:13:06 +0530

    first time adding in github

---
+++ addnewnumbers.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Adding the two numbers
+Sum = num1 + num2
+
+# Printing the result
+print("The sum of {0} and {1} is {2}".format(num1, num2, Sum))
